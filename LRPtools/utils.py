7cdb49fdb591db9ebaade29de82e4d9e37b156f6
Sun Jiamei <sunjiamei.hit@gmail.com>
2020-07-21 09:29:09 +0800

    add GNN net

---
+++ LRPtools/utils.py
@@ -0,0 +1,196 @@
+import torch.nn as nn
+import torch
+import numpy as np
+import matplotlib.pyplot as plt
+from PIL import Image
+import skimage.transform
+LOWEST = -1
+HIGHEST = 1
+EPSILON = 0.01
+Z_EPSILON = 1e-7
+LOGIT_BETA = 4
+
+def safe_divide(numerator, divisor):
+    # Save divide in iNNvestigate: a / (b + iK.to_floatx(K.equal(b, K.constant(0))) * K.epsilon())
+    return numerator / (divisor + Z_EPSILON * (divisor == 0).float())
+
+
+def project(X, output_range=(0, 1), absmax=None, input_is_postive_only=False):
+
+    if absmax is None:
+        absmax = np.max(np.abs(X),
+                        axis=tuple(range(1, len(X.shape))))
+    absmax = np.asarray(absmax)
+    # print(absmax.shape)
+    # print(X.shape)
+    mask = absmax != 0
+    # print(mask)
+    if mask.sum() > 0:
+        X[mask] /= absmax[mask]
+
+    if input_is_postive_only is False:
+        X = (X+1)/2  # [0, 1]
+    X = X.clip(0, 1)
+
+    X = output_range[0] + (X * (output_range[1]-output_range[0]))
+    return X
+
+
+def normalize_relevance(X, dim=-1):
+    # X = torch.clamp(X, min=0)
+
+    value, indice = torch.max(torch.abs(X), dim=dim)
+    value.masked_fill_(value==0, 1)
+    # print(value)
+    # print(X.shape)
+    X = X/value.unsqueeze(dim)
+    # X.masked_fill_(X==0, 1)
+    return X + 1
+
+
+
+
+
+def heatmap(X, cmap_type="seismic", reduce_op="sum", reduce_axis=-1, **kwargs):
+    cmap = plt.cm.get_cmap(cmap_type)
+
+    tmp = X
+    shape = tmp.shape
+
+    if reduce_op == "sum":
+        tmp = tmp.sum(axis=reduce_axis)
+    elif reduce_op == "absmax":
+        pos_max = tmp.max(axis=reduce_axis)
+        neg_max = (-tmp).max(axis=reduce_axis)
+        abs_neg_max = -neg_max
+        tmp = np.select([pos_max >= abs_neg_max, pos_max < abs_neg_max],
+                        [pos_max, neg_max])
+    else:
+        raise NotImplementedError()
+    # print(tmp.shape)
+    tmp = project(tmp, output_range=(0, 255), **kwargs).astype(np.int64)
+
+    tmp = cmap(tmp.flatten())[:, :3].T
+    tmp = tmp.T
+
+    shape = list(shape)
+    shape[reduce_axis] = 3
+    return tmp.reshape(shape).astype(np.float32)
+
+
+def graymap(X, **kwargs):
+    return heatmap(X, cmap_type="gray", **kwargs)
+
+
+def gamma(X, gamma = 0.5, minamp=0, maxamp=None):
+    """
+    apply gamma correction to an input array X
+    while maintaining the relative order of entries,
+    also for negative vs positive values in X.
+    the fxn firstly determines the max
+    amplitude in both positive and negative
+    direction and then applies gamma scaling
+    to the positive and negative values of the
+    array separately, according to the common amplitude.
+
+    :param gamma: the gamma parameter for gamma scaling
+    :param minamp: the smallest absolute value to consider.
+    if not given assumed to be zero (neutral value for relevance,
+        min value for saliency, ...). values above and below
+        minamp are treated separately.
+    :param maxamp: the largest absolute value to consider relative
+    to the neutral value minamp
+    if not given determined from the given data.
+    """
+
+    #prepare return array
+    # print(X.shape)
+    Y = np.zeros_like(X)
+    # print(Y.shape)
+
+    X = X - minamp # shift to given/assumed center
+    if maxamp is None: maxamp = np.abs(X).max() #infer maxamp if not given
+    X = X / maxamp # scale linearly
+
+    #apply gamma correction for both positive and negative values.
+    i_pos = X >= 0
+    i_neg = np.invert(i_pos)
+    Y[i_pos] = X[i_pos]**gamma
+    Y[i_neg] = -(-X[i_neg])**gamma
+
+    #reconstruct original scale and center
+    Y *= maxamp
+    Y += minamp
+
+    return Y
+
+
+def visuallize_attention(image, attention, reshape_size, cmap_type="seismic",):
+    '''this function will blend the original highlightened by the attention
+        the input images are with shape (channel, height, width) an Image object
+        the attentions are with shape (1, height*width)) a pytorch tensor'''
+
+    def project_inside(x):
+        absmax = np.max(np.abs(x))
+        x = 1.0 * x / absmax
+        if np.sum(x < 0):
+            x = (x + 1) / 2
+        else:
+            x = x
+        return x
+    attention = attention.view(reshape_size)
+    attention = attention.cpu().detach().numpy()-1
+    # print(attention.shape)
+    attention = project_inside(attention)
+    # print(attention)
+    atn = skimage.transform.pyramid_expand(attention, upscale=14,
+                                           multichannel=False)
+    # print(atn)
+    # plt.imshow(atn)
+    # plt.show()
+    cm = plt.get_cmap('jet')
+    atn_heatmap = cm(atn)
+    # print(atn_heatmap[:,:,0])
+    # plt.imshow(atn_heatmap[:,:,:3])
+    # plt.show()
+    # print(atn.shape)
+    attention_heatmap = Image.fromarray(np.uint8(atn_heatmap[:, :, :3]*255))
+    # attention_heatmap.show()
+    merged_heatmap = Image.blend(image, attention_heatmap, 0.5)
+    return merged_heatmap
+
+
+
+
+def compute_lrp_sum(sum_output, sum_input, relevance_sum_output,dim=-1):
+    #this function will return the relevance of the sum_input
+    # print(sum_output)
+    # print(torch.sum(sum_input,dim=dim))
+    # print(sum_output, torch.sum(sum_input, dim=dim))
+    assert (sum_output == torch.sum(sum_input,dim=dim)).all()
+    fea_dim = sum_input.size()[-1]
+    # print(relevance_sum_output.unsqueeze(-1).repeat(1,1,1,fea_dim).shape)
+    # print(sum_input.shape)
+    relevance = relevance_sum_output.unsqueeze(-1).repeat(1,1,1,fea_dim)
+    # print(relevance)
+    out = sum_output.unsqueeze(-1).repeat(1,1,1,fea_dim)
+    mask = out == 0
+    out.masked_fill_(mask, 1 / fea_dim)
+    relevance_sum_input = relevance * sum_input / (out + util.EPSILON * out.sign())
+    return relevance_sum_input
+
+def compute_lrp_mean(mean_output, mean_input, relevance_mean_output,dim=-1):
+    #this function will return the relevance of the mean_input
+    assert (mean_output == torch.mean(mean_input,dim=dim)).all()
+    fea_dim = mean_input.size()[-1]
+    input_dim = len(mean_input.shape)
+    repeat_param = [1]*input_dim
+    repeat_param[-1] *= fea_dim
+    # print(repeat_param)
+    relevance = relevance_mean_output.unsqueeze(-1).repeat(repeat_param)
+    out = mean_input.sum(dim=dim).unsqueeze(-1).repeat(repeat_param)
+    mask = out == 0
+    # input = mean_input / fea_dim
+    out.masked_fill_(mask, 1/fea_dim)
+    relevance_mean_input = relevance * mean_input / (out + util.EPSILON * out.sign())
+    return relevance_mean_input
---
+++ fewshot-CAN/net.py
@@ -0,0 +1,171 @@
+import math
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+from torchFewShot.LRPtools import utils as LRPutil
+from .resnet12 import resnet12
+from .cam import CAM
+
+
+class Model(nn.Module):
+    def __init__(self, scale_cls, num_classes=64):
+        super(Model, self).__init__()
+        self.scale_cls = scale_cls
+
+        self.base = resnet12()
+        self.cam = CAM()
+
+        self.nFeat = self.base.nFeat
+        self.clasifier = nn.Conv2d(self.nFeat, num_classes, kernel_size=1) 
+
+    def test(self, ftrain, ftest):
+        ftest = ftest.mean(4)
+        ftest = ftest.mean(4)
+        ftest = F.normalize(ftest, p=2, dim=ftest.dim()-1, eps=1e-12)
+        ftrain = F.normalize(ftrain, p=2, dim=ftrain.dim()-1, eps=1e-12)
+        scores = self.scale_cls * torch.sum(ftest * ftrain, dim=-1)
+        return scores
+
+    def extract_feature(self, x):
+        x = x.unsqueeze(0)
+        f = self.base(x)
+        return f
+
+    def forward(self, xtrain, xtest, ytrain, ytest):
+        batch_size, num_train = xtrain.size(0), xtrain.size(1)
+        num_test = xtest.size(1)
+        K = ytrain.size(2)
+        ytrain = ytrain.transpose(1, 2)
+
+        xtrain = xtrain.view(-1, xtrain.size(2), xtrain.size(3), xtrain.size(4))
+        xtest = xtest.view(-1, xtest.size(2), xtest.size(3), xtest.size(4))
+        x = torch.cat((xtrain, xtest), 0)
+        f = self.base(x)
+        f_h = f.size(-2)
+        f_w = f.size(-1)
+        ftrain = f[:batch_size * num_train]
+        ftrain = ftrain.view(batch_size, num_train, -1)
+        ftrain = torch.bmm(ytrain, ftrain)
+        ftrain = ftrain.div(ytrain.sum(dim=2, keepdim=True).expand_as(ftrain))
+        ftrain = ftrain.view(batch_size, -1, *f.size()[1:])
+        ftest = f[batch_size * num_train:]
+        ftest = ftest.view(batch_size, num_test, *f.size()[1:])
+        ftrain, ftest, _, _ = self.cam(ftrain, ftest)
+        ftrain = ftrain.mean(4)
+        ftrain = ftrain.mean(4)
+
+        if not self.training:
+            return self.test(ftrain, ftest)
+
+        ftest_norm = F.normalize(ftest, p=2, dim=3, eps=1e-12)
+        ftrain_norm = F.normalize(ftrain, p=2, dim=3, eps=1e-12)
+        ftrain_norm = ftrain_norm.unsqueeze(4)
+        ftrain_norm = ftrain_norm.unsqueeze(5)
+        cls_scores = self.scale_cls * torch.sum(ftest_norm * ftrain_norm, dim=3)
+        cls_scores = cls_scores.view(batch_size * num_test, *cls_scores.size()[2:])
+
+        ftest = ftest.view(batch_size, num_test, K, -1)
+        ftest = ftest.transpose(2, 3)
+        ytest = ytest.unsqueeze(3)
+        ftest = torch.matmul(ftest, ytest)
+        ftest = ftest.view(batch_size * num_test, -1, f_h, f_w)
+        ytest = self.clasifier(ftest)
+
+        return ytest, cls_scores
+
+
+
+'''LRP weighted'''
+
+class ModelwithLRP(nn.Module):
+    def __init__(self, scale_cls, num_classes=64):
+        super(ModelwithLRP, self).__init__()
+        self.scale_cls = scale_cls
+
+        self.base = resnet12()
+        self.cam = CAM()
+
+        self.nFeat = self.base.nFeat
+        self.clasifier = nn.Conv2d(self.nFeat, num_classes, kernel_size=1)
+
+    def test(self, ftrain, ftest):
+        ftest = ftest.mean(-1)
+        ftest = ftest.mean(-1)
+        ftest = F.normalize(ftest, p=2, dim=ftest.dim()-1, eps=1e-12)
+        ftrain = F.normalize(ftrain, p=2, dim=ftrain.dim()-1, eps=1e-12)
+
+        scores = self.scale_cls * torch.sum(ftest * ftrain, dim=-1)
+        return scores
+
+    def forward(self, xtrain, xtest, ytrain, ytest):
+        batch_size, num_train = xtrain.size(0), xtrain.size(1)
+        num_test = xtest.size(1)
+        K = ytrain.size(2)
+        ytrain = ytrain.transpose(1, 2)
+
+        xtrain = xtrain.view(-1, xtrain.size(2), xtrain.size(3), xtrain.size(4))
+        xtest = xtest.view(-1, xtest.size(2), xtest.size(3), xtest.size(4))
+        x = torch.cat((xtrain, xtest), 0)
+        f = self.base(x)
+
+        ftrain = f[:batch_size * num_train]
+        ftrain = ftrain.view(batch_size, num_train, -1)
+        ftrain = torch.bmm(ytrain, ftrain)
+        ftrain = ftrain.div(ytrain.sum(dim=2, keepdim=True).expand_as(ftrain))
+        ftrain = ftrain.view(batch_size, -1, *f.size()[1:])
+        ftest = f[batch_size * num_train:]
+        ftest = ftest.view(batch_size, num_test, *f.size()[1:])
+        ftrain, ftest, _, _ = self.cam(ftrain, ftest)
+
+
+        ftest_att = ftest
+        ftrain = ftrain.mean(-1)
+        ftrain = ftrain.mean(-1)
+        ftrain_mean2 = ftrain
+
+        if not self.training:
+            return self.test(ftrain, ftest)
+        ftest_norm = F.normalize(ftest, p=2, dim=3, eps=1e-12)
+        ftrain_norm = F.normalize(ftrain, p=2, dim=3, eps=1e-12)
+        ftrain_norm = ftrain_norm.unsqueeze(4)
+        ftrain_norm = ftrain_norm.unsqueeze(5)
+
+        ftest_mean1 = ftest_att.mean(-1)
+        # print(ftest.sum(), ftest_mean1.sum())
+        ftest_mean2 = ftest_mean1.mean(-1)  # (b, n2, n1, c)
+        # print(ftest_mean2.sum())
+        ftest_mean2_norm = F.normalize(ftest_mean2, p=2, dim=ftest_mean2.dim() - 1,
+                                       eps=1e-12)  # this is the attended test features, each test sample corresponds to a set of features
+        ftrain_mean2_norm = F.normalize(ftrain_mean2, p=2, dim=ftrain_mean2.dim() - 1,
+                                        eps=1e-12)  # this is the attended centroid, each test sample corresponds to a set of centroids
+
+        scores = self.scale_cls * torch.sum(ftest_mean2_norm * ftrain_mean2_norm, dim=-1)  #(b, n2, n1)
+
+        # calculate the logits from the cosine similarity
+        logits_sf = torch.softmax(scores, dim=-1)
+        # initialize the relevance of each label
+        relevance_logits = torch.log(LRPutil.LOGIT_BETA * (logits_sf +LRPutil.EPSILON)/ (torch.tensor([1 + LRPutil.EPSILON]).cuda() - logits_sf))
+
+        relevance_ftest_mul_ftrain = LRPutil.compute_lrp_sum(
+            torch.sum(ftest_mean2_norm * ftrain_mean2_norm, dim=-1),
+            ftest_mean2_norm * ftrain_mean2_norm,
+            relevance_logits, dim=-1)  #(b, n2, n1, c)
+        relevance_weight = LRPutil.normalize_relevance(relevance_ftest_mul_ftrain.squeeze(-1))
+
+        relevance_ftest_mean1 = LRPutil.compute_lrp_mean(ftest_mean2, ftest_mean1, relevance_ftest_mul_ftrain, dim=-1)
+        relevance_ftest = LRPutil.compute_lrp_mean(ftest_mean1, ftest_att, relevance_ftest_mean1, dim=-1)  #(b, n2, n1, c, h, w)
+
+        relevance_ftest = LRPutil.normalize_relevance(relevance_ftest,dim=3)
+
+        cls_scores = self.scale_cls * torch.sum(ftest_norm * ftrain_norm * relevance_ftest * relevance_weight.unsqueeze(-1).unsqueeze(-1), dim=3)
+        cls_scores = cls_scores.view(batch_size * num_test, *cls_scores.size()[2:])
+
+
+        ftest = ftest.view(batch_size, num_test, K, -1)
+        ftest = ftest.transpose(2, 3)
+        ytest = ytest.unsqueeze(3)
+        ftest = torch.matmul(ftest, ytest)
+        ftest = ftest.view(batch_size * num_test, -1, 6, 6)
+        ytest = self.clasifier(ftest)
+        return ytest, cls_scores
+
